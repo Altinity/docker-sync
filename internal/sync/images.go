a793b589ec01b234272b7eb8c39ab9a579ea19d3
Henrique Goncalves <kamus@hadenes.io>
2024-06-14 11:12:38 -0300

    add backoff

--- internal/sync/images.go
+++ internal/sync/images.go
@@ -3,11 +3,13 @@ package sync
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/Altinity/docker-sync/config"
 	"github.com/Altinity/docker-sync/internal/telemetry"
 	"github.com/Altinity/docker-sync/structs"
+	"github.com/cenkalti/backoff/v4"
 	"github.com/rs/zerolog/log"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
@@ -38,8 +40,23 @@ func SyncImage(ctx context.Context, image *structs.Image) error {
 			}
 		}
 
-		if serr := SyncTag(image, tag, pullAuthName, pullAuth); serr != nil {
-			errs := multierr.Errors(serr)
+		if err := backoff.Retry(func() error {
+			if err := SyncTag(image, tag, pullAuthName, pullAuth); err != nil {
+				if strings.Contains(err.Error(), "HAP429") {
+					log.Warn().
+						Str("source", image.Source).
+						Msg("Rate limited by registry, backing off")
+					return err
+				}
+
+				return backoff.Permanent(err)
+			}
+
+			return nil
+		}, backoff.NewExponentialBackOff(
+			backoff.WithInitialInterval(1*time.Minute),
+		)); err != nil {
+			errs := multierr.Errors(err)
 			if len(errs) > 0 {
 				telemetry.Errors.Add(ctx, int64(len(errs)),
 					metric.WithAttributes(
@@ -56,10 +73,10 @@ func SyncImage(ctx context.Context, image *structs.Image) error {
 				log.Error().
 					Errs("errors", errs).
 					Msg("Failed to sync tag")
-			}
 
-			merr = multierr.Append(merr, serr)
-			continue
+				merr = multierr.Append(merr, err)
+				continue
+			}
 		}
 		lastSyncMap[k] = time.Now()
 	}
